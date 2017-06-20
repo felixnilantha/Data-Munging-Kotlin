9326fa3aaef36d866b338a409814f8e572840d74
Nilantha Silva <felixnilantha@gmail.com>
2017-06-20 22:50:01 +1000

    Add data class WeatherEntry

--- src/org/kotlin/datamunging/DataMunging.kt
+++ src/org/kotlin/datamunging/DataMunging.kt
@@ -7,11 +7,13 @@ class DataMunging : StringSpec() {
 	init {
 		val lamda: () -> Unit = {
 			val lines: List<String> = File("weather.dat").readLines().drop(2).take(30)
-			val splitlines: List <List<Any>> = lines.map { it.replace("*", "").split(Regex(" +")) }
-					.map { listOf(it[1], it[2].toInt(), it[3].toInt())}
+			val splitlines: List <WeatherEntry> = lines.map { it.replace("*", "").split(Regex(" +")) }
+					.map { WeatherEntry(it[1], it[2].toInt(), it[3].toInt())}
 		
 			splitlines.forEach { println(it) }
 		}
 		"Find day with minimum temperature".invoke(lamda)
 	}
 }
+
+data class WeatherEntry(val day: String, val max: Int, val min: Int)
