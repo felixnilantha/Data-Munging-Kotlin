086e6885018207a4e85dd53e8d9cc3dd88885dfd
Nilantha Silva <felixnilantha@gmail.com>
2017-06-20 23:21:35 +1000

    Add comments before refactoring

--- src/org/kotlin/datamunging/DataMunging.kt
+++ src/org/kotlin/datamunging/DataMunging.kt
@@ -4,8 +4,12 @@ import java.io.File
 import io.kotlintest.specs.StringSpec
 import io.kotlintest.matchers.shouldEqual
 
+
+/// this is the code before refactring will be commited for reference purpose to
+/// whome need to understand lamda 
 class DataMunging : StringSpec() {
 	init {
+		
 		val lamda: () -> Unit = {
 			val lines: List<String> = File("weather.dat").readLines().drop(2).take(30)
 			val splitlines: List <WeatherEntry> = lines.map { it.replace("*", "").split(Regex(" +")) }
