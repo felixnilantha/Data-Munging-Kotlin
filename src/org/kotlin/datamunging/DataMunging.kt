1a5f391df6f9b4585e4840b1576ee300737496e4
Nilantha Silva <felixnilantha@gmail.com>
2017-06-20 23:18:33 +1000

    tested with min temperature in day 14

--- src/org/kotlin/datamunging/DataMunging.kt
+++ src/org/kotlin/datamunging/DataMunging.kt
@@ -2,15 +2,16 @@ package org.kotlin.datamunging
 
 import java.io.File
 import io.kotlintest.specs.StringSpec
+import io.kotlintest.matchers.shouldEqual
 
 class DataMunging : StringSpec() {
 	init {
 		val lamda: () -> Unit = {
 			val lines: List<String> = File("weather.dat").readLines().drop(2).take(30)
 			val splitlines: List <WeatherEntry> = lines.map { it.replace("*", "").split(Regex(" +")) }
-					.map { WeatherEntry(it[1], it[2].toInt(), it[3].toInt())}
-		
-			splitlines.forEach { println(it) }
+					.map { WeatherEntry(it[1], it[2].toInt(), it[3].toInt()) }
+			val entry: WeatherEntry? =splitlines.minBy { it.max - it.min }
+			entry!!.day shouldEqual "14"
 		}
 		"Find day with minimum temperature".invoke(lamda)
 	}
